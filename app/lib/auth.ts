7d1171026aaf3e34be3ca43106284f94c2c6a1a6
Rostyslav Voitovych <r.voitovych@gmail.com>
2025-01-07 13:50:02 +0200

    auth with api key for server

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,3 +1,7 @@
+import { NextRequest } from "next/server";
+import { env } from '@/app/config/env';
+import allowedUsersJSON from '@/app/config/allowed-users.json';
+
 export const isAuthorized = ({
   actualAuthToken,
   expectedAuthToken,
@@ -5,3 +9,23 @@ export const isAuthorized = ({
   actualAuthToken: string | null;
   expectedAuthToken: string;
 }) => actualAuthToken === expectedAuthToken;
+
+export const isAllowedServerCommunication = (request: NextRequest): boolean => {
+  const isTokenCorrect = request.headers.get("x-api-key") === env.API_TOKEN;
+  const isPlaywright = request.headers.get("User-Agent")?.includes("playwright") ?? false
+
+  const allowedRoutes = ['/api/result/upload', 'api/config'];
+
+  const isAllowedRoute = allowedRoutes.some((route) => request.nextUrl.pathname.includes(route));
+
+  return isTokenCorrect && isPlaywright && isAllowedRoute;
+}
+
+export const isRecognizedOrganization = (unique_name: string) => {
+  if (!unique_name?.includes('@')) {
+    return false
+  }
+
+  const domain = unique_name.slice(unique_name.indexOf('@') + 1)
+  return allowedUsersJSON.allowedDomains.includes(domain);
+}
--- middleware.ts
+++ middleware.ts
@@ -6,13 +6,15 @@ import { getToken } from 'next-auth/jwt';
 
 import jwt from 'jsonwebtoken';
 
-import allowedUsersJSON from '@/app/config/allowed-users.json';
-
-import { isAuthorized } from '@/app/lib/auth';
+import { isAllowedServerCommunication, isRecognizedOrganization } from '@/app/lib/auth';
 
 export const config = {
   matcher: '/api/:path*',
 };
+
+interface CustomJwtPayload extends jwt.JwtPayload {
+  unique_name: string;
+}
 
 export async function middleware(request: NextRequest) {
   const isAuthRequired = !!env.API_TOKEN;
@@ -27,16 +29,9 @@ export async function middleware(request: NextRequest) {
     secureCookie: !!env.NEXTAUTH_URL?.includes('https')
   });
 
-  interface CustomJwtPayload extends jwt.JwtPayload {
-    unique_name: string;
-  }
 
   const decoded = jwt.decode(token?.access_token as string) as CustomJwtPayload;
 
-  const isRecognizedOrganization = (unique_name: string) => {
-    const domain = unique_name.slice(unique_name.indexOf('@') + 1)
-    return allowedUsersJSON.allowedDomains.includes(domain);
-  }
 
   const unprotectedRoutes = ['/api/ping', '/api/auth/', '/api/serve/', '/api/static/'];
 
@@ -46,10 +41,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  const actualAuthToken = request.headers.get('Authorization');
-  const expectedAuthToken = env.API_TOKEN!;
-
-  if (!isAuthorized({ actualAuthToken, expectedAuthToken }) && !isRecognizedOrganization(decoded?.unique_name)) {
+  if (!isAllowedServerCommunication(request) && !isRecognizedOrganization(decoded?.unique_name)) {
     return CommonResponseFactory.buildUnauthorizedResponse();
   }
 }
